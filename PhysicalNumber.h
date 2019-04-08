a95117ff577d47958de778c5a29b01677effbc2a
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-08 23:00:51 +0300

    Add files via upload

---
+++ PhysicalNumber.h
@@ -0,0 +1,44 @@
+#ifndef PHYSICALNUMBER_H
+#define PHYSICALNUMBER_H
+#include <iostream>
+#include "Unit.h"
+
+using namespace std;
+
+namespace ariel {
+
+class PhysicalNumber{
+    private:
+    Unit u;
+    double num;
+    
+    public:
+    PhysicalNumber(double num,Unit u);
+    PhysicalNumber(const PhysicalNumber& pn);
+
+    const PhysicalNumber operator+(const PhysicalNumber&); 
+	const PhysicalNumber operator-(const PhysicalNumber&);
+	PhysicalNumber& operator+=(const PhysicalNumber&);
+	PhysicalNumber& operator-=(const PhysicalNumber&);
+	PhysicalNumber operator+(); // Unari
+	PhysicalNumber operator-(); // Unari
+
+    bool operator> (const PhysicalNumber&) ;
+    bool operator< (const PhysicalNumber&) ;
+    bool operator>= (const PhysicalNumber&) ;
+    bool operator<= (const PhysicalNumber&) ;
+    bool operator== (const PhysicalNumber&) ;
+    bool operator!= (const PhysicalNumber&) ;
+    
+    PhysicalNumber operator++();//++i
+    PhysicalNumber operator++(int);//i++
+    PhysicalNumber operator--();//--i
+    PhysicalNumber operator--(int);//i--
+    
+    
+    friend ostream& operator<< (ostream& os, const PhysicalNumber& c);//פלט
+    friend istream& operator>> (istream& is, PhysicalNumber& c);//קלט
+    
+    };
+}
+#endif
---
+++ Unit.h
@@ -0,0 +1,12 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+namespace ariel{
+    
+    enum Unit {
+		KM, M, CM,
+		HOUR, MIN, SEC,
+		TON, KG, G
+    };
+}//end namespace
+#endif
