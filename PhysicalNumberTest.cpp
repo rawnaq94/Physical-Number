9cf3cd1ba842c807aa19d7bf7e8fb1502aa64d33
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-09 00:04:24 +0300

    Update PhysicalNumberTest.cpp

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -65,12 +65,15 @@ int main() {
    // PRINT OUTPUT
       
     .setname("Basic output")
-    .CHECK_OUTPUT(g, "4[ton]")
-      
     .CHECK_OUTPUT(e, "4000[cm]")
-    .CHECK_OUTPUT(i, "700[g]")
-    .CHECK_OUTPUT(h, "40[kg]")
     .CHECK_OUTPUT(f, "80[sec]")
+    .CHECK_OUTPUT(g, "4[ton]")
+    .CHECK_OUTPUT(h, "40[kg]") 
+    .CHECK_OUTPUT(i, "700[g]")
+    .CHECK_OUTPUT(l, "3[km]")
+    .CHECK_OUTPUT(m, "20[m]")
+    .CHECK_OUTPUT(n, "7[hour]")
+    .CHECK_OUTPUT(o, "60[min]")
     
 
   // operator (+) && operator (-)
@@ -78,30 +81,45 @@ int main() {
       
     .setname("Compatible dimensions")
     .CHECK_OUTPUT(e+e, "8000[cm]")
+    .CHECK_OUTPUT(f+f, "160[sec]")
+    .CHECK_OUTPUT(g+g, "8[ton]") 
     .CHECK_OUTPUT(f++, "81[sec]")
+    .CHECK_OUTPUT(n++, "8[hour]")   
     .CHECK_OUTPUT(++i, "701[g]")
     .CHECK_OUTPUT(--i, "700[g]")
+    .CHECK_OUTPUT(++m, "21[m]")
+    .CHECK_OUTPUT(--m, "20[m]")
     .CHECK_OUTPUT((h-=i), "39.3[kg]")
+      
       
   // error calculations    
 
     .setname("Incompatible dimensions")
+    .CHECK_THROWS(o+m)
+    .CHECK_THROWS(l+i)
     .CHECK_THROWS(e+f)
     .CHECK_THROWS(e+g)
     .CHECK_THROWS(h-f)
+    .CHECK_THROWS(f-b)
     .CHECK_THROWS(g-=f)
+    .CHECK_THROWS(h-=e)
     .CHECK_THROWS(e+g)
     .CHECK_THROWS(h-f)
     .CHECK_THROWS(i-e)
     .CHECK_THROWS(i>f)
-    .CHECK_THROWS(g==e)
+    .CHECK_THROWS(i==e)
     .CHECK_THROWS(e<=f)
     
+      
     .setname("Check OK")
     .CHECK_OK(istringstream("700[g]")>>i)
     .CHECK_OK(istringstream("8000[cm]")>>e)
+    .CHECK_OK(istringstream("160[sec]")>>f)
+    .CHECK_OK(istringstream("8[ton]")>>g)
     .CHECK_OK(istringstream("81[sec]")>>f)
+    .CHECK_OK(istringstream("8[hour]")>>n)
     .CHECK_OK(istringstream("39.3[kg]")>>h)
+    .CHECK_OK(istringstream("20[m]")>>m)
 
       
     .setname("Compatible dimensions")
