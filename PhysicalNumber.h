f7a914fe39eb300ccb61f2b25028751f831942cb
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-09 00:26:38 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,44 +1,48 @@
+#include <iostream>
 #ifndef PHYSICALNUMBER_H
 #define PHYSICALNUMBER_H
-#include <iostream>
 #include "Unit.h"
-
 using namespace std;
 
-namespace ariel {
+namespace ariel
+{
 
-class PhysicalNumber{
-    private:
-    Unit _x;
-    double number;
+    class PhysicalNumber
+    {
     
-    public:
-    PhysicalNumber(double number,Unit _x);
-    PhysicalNumber(const PhysicalNumber& pn);
+    
+       public:
+       PhysicalNumber(double number,Unit _x);
+       PhysicalNumber(const PhysicalNumber& pn);
 
-    const PhysicalNumber operator+(const PhysicalNumber&); 
-	const PhysicalNumber operator-(const PhysicalNumber&);
-	PhysicalNumber& operator+=(const PhysicalNumber&);
-	PhysicalNumber& operator-=(const PhysicalNumber&);
-	PhysicalNumber operator+(); // Unari
-	PhysicalNumber operator-(); // Unari
+       const PhysicalNumber operator+(const PhysicalNumber&); 
+       const PhysicalNumber operator-(const PhysicalNumber&);
+       PhysicalNumber& operator+=(const PhysicalNumber&);
+       PhysicalNumber& operator-=(const PhysicalNumber&);
+       PhysicalNumber operator+(); // Unari
+       PhysicalNumber operator-(); // Unari
 
-    bool operator> (const PhysicalNumber&) ;
-    bool operator< (const PhysicalNumber&) ;
-    bool operator>= (const PhysicalNumber&) ;
-    bool operator<= (const PhysicalNumber&) ;
-    bool operator== (const PhysicalNumber&) ;
-    bool operator!= (const PhysicalNumber&) ;
+       bool operator> (const PhysicalNumber&) ;
+       bool operator< (const PhysicalNumber&) ;
+       bool operator>= (const PhysicalNumber&) ;
+       bool operator<= (const PhysicalNumber&) ;
+       bool operator== (const PhysicalNumber&) ;
+       bool operator!= (const PhysicalNumber&) ;
     
-    PhysicalNumber operator++();//++i
-    PhysicalNumber operator++(int);//i++
-    PhysicalNumber operator--();//--i
-    PhysicalNumber operator--(int);//i--
+       PhysicalNumber operator++();//++i
+       PhysicalNumber operator++(int);//i++
+       PhysicalNumber operator--();//--i
+       PhysicalNumber operator--(int);//i--
     
     
-    friend ostream& operator<< (ostream& os, const PhysicalNumber& c);//פלט
-    friend istream& operator>> (istream& is, PhysicalNumber& c);//קלט
+       friend ostream& operator<< (ostream& os, const PhysicalNumber& c);//פלט
+       friend istream& operator>> (istream& is, PhysicalNumber& c);//קלט
     
+       private:
+                Unit _x;
+                double number; 
+	
     };
+	
 }
 #endif
