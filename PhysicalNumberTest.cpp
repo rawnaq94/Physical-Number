b7d052bacf19b54c5982db160374876f933abe6d
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-08 22:40:48 +0300

    Update PhysicalNumberTest.cpp

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -30,9 +30,9 @@ int main() {
     PhysicalNumber cc(10, Unit::M);
     PhysicalNumber dd(40, Unit::KG);
     PhysicalNumber ee(200, Unit::G);
-    PhysicalNumber ff(20, Unit::TON);
+    PhysicalNumber ff(2, Unit::TON);
     PhysicalNumber gg(5, Unit::HOUR);
-    PhysicalNumber hh(300, Unit::SEC);
+    PhysicalNumber hh(30, Unit::SEC);
     PhysicalNumber ii(60, Unit::MIN); 
     
 
@@ -69,9 +69,9 @@ int main() {
      .CHECK_OUTPUT(cc, "10[m]")
      .CHECK_OUTPUT(dd, "40[kg]")
      .CHECK_OUTPUT(ee, "200[g]")
-     .CHECK_OUTPUT(ff, "20[ton]")
+     .CHECK_OUTPUT(ff, "2[ton]")
      .CHECK_OUTPUT(gg, "5[hour]")
-     .CHECK_OUTPUT(hh, "300[sec]")
+     .CHECK_OUTPUT(hh, "30[sec]")
      .CHECK_OUTPUT(ii, "60[min]") 
       
       
@@ -79,46 +79,51 @@ int main() {
      // opratot (+=) && opratot (-=)
      // operator (++) && opertor (--)
       
-     .setname("Compatible dimensions") 
-     .CHECK_OUTPUT(aa+=bb, "12.06[km]") 
-     .CHECK_OUTPUT(aa, "12.06[km]")
-     .CHECK_OUTPUT(dd++, "41[kg]")
-     .CHECK_OUTPUT(dd, "41[kg]")
-     .CHECK_OUTPUT(gg+=ii, "6[hour]")
-     .CHECK_OUTPUT(gg, "6[hour]")
+     .setname("Compatible dimensions")
+     .CHECK_OUTPUT(bb+bb, "12000[cm]")
+     .CHECK_OUTPUT(ff-=dd, "19.96[ton]") 
+     .CHECK_OUTPUT(ff, "19.96[ton]") 
+     .CHECK_OUTPUT(hh++, "31[sec]")
+     .CHECK_OUTPUT(++ee, "201[g]")
+     .CHECK_OUTPUT(--ee, "200[g]")
+     .CHECK_OUTPUT(dd-=ee, "39.8[kg]")
       
-     .CHECK_OUTPUT(bb-=cc, "5000[cm]")
-     .CHECK_OUTPUT(bb, "5000[cm]") 
-     .CHECK_OUTPUT(ff--, "19[ton]") 
-     .CHECK_OUTPUT(ff, "19[ton]") 
+     
+     .setname("Incompatible dimensions")
+     .CHECK_THROWS(bb+hh)
+     .CHECK_THROWS(bb+ff)
+     .CHECK_THROWS(dd-hh)
+     .CHECK_THROWS(ff-=hh)
+     .CHECK_THROWS(ee-bb)
+     .CHECK_THROWS(ee>hh)
+     .CHECK_THROWS(ff==bb)
+     .CHECK_THROWS(bb<=hh)
       
       
-      
+       
      .setname("Check OK")
-     .CHECK_OK(istringstream("12.06[km]")>>aa)
-     .CHECK_OK(istringstream("41[kg]")>>dd)
-     .CHECK_OK(istringstream("6[hour]")>>gg)
-     .CHECK_OK(istringstream("19[ton]")>>ff)
+     .CHECK_OK(istringstream("200[g]")>>ee)
+     .CHECK_OK(istringstream("12000[cm]")>>bb)
+     .CHECK_OK(istringstream("31[sec]")>>hh)
+     .CHECK_OK(istringstream("39.8[kg]")>>dd)
       
       
     // opratot (<) && opratot (>)  
    // opratot (==) && opratot (!=) 
-      
-     .CHECK_EQUAL(bb>aa,false)
-     .CHECK_EQUAL(ff==dd,false)
-     .CHECK_OK(istringstream("10000[kg]")>>dd)
+     
+     .setname("Compatible dimensions") 
+     .CHECK_EQUAL(hh>d,false)
+     .CHECK_EQUAL(dd==ff,false)
+     .CHECK_OK(istringstream("1000[kg]")>>dd)
      .CHECK_OUTPUT(dd, "10000[kg]")
-     .CHECK_EQUAL(aa!=cc,true)
-     .CHECK_EQUAL(hh>ii,false)
-     .CHECK_EQUAL(ff<dd,false)
-     .CHECK_EQUAL(ee<ff,true)
-     .CHECK_EQUAL(cc<bb,true)
-     .CHECK_EQUAL(hh<=ii,true) 
-     .CHECK_EQUAL(ii>=hh,true) 
+     .CHECK_EQUAL(ee!=dd,true)
+     .CHECK_EQUAL(ff>ee,true)
+     .CHECK_EQUAL(ee>dd,false)
+     .CHECK_EQUAL(bb==a,false)
+     .CHECK_EQUAL(c<d,true)
+    
       
-      
-
-
+    
       .print(cout, /*show_grade=*/false);
       grade = testcase.grade();
     } else {
